9d19ac06210eac332b8c05a06c232bd147b847bf
MakMaka <62114682+MakMaka@users.noreply.github.com>
2020-05-21 20:49:02 +0300

    .
    .

--- Assets/Script2/ButtonHome.cs
+++ Assets/Script2/ButtonHome.cs
@@ -24,6 +24,7 @@ public class ButtonHome : MonoBehaviour
         switch (gameObject.name)
         {
             case "Home":
+                GameObject.Find("NatureAudio").GetComponent<AudioSource>().Stop();
                 Application.LoadLevel("Menu");
                 break;
         }
